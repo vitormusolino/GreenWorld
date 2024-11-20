2c995706a980bf7a37a285e8645d852e1e3f0cac
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2024-11-20 11:08:30 -0300

    Add files via upload

--- src/pages/Comunidade.jsx
+++ src/pages/Comunidade.jsx
@@ -1,8 +1,12 @@
 import ComunidadeComp from '../components/Comunidade/Comunidade'
+import EnergiaFav from '../components/EnergiaFav/EnergiaFav';
+import Footer from '../components/Footer/Footer'
 
 function Comunidade() {
     return ( <>
                 <ComunidadeComp/>
+                <EnergiaFav/>
+                <Footer/>
             </> );
 }
 
